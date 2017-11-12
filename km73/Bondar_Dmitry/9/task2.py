aefe3522ffe29835b6ce8751e9731f09b4cab09e
Dima Bondar <31803588+dmitrypm@users.noreply.github.com>
2017-11-12 17:04:14 +0200

    Update task2.py

--- km73/Bondar_Dmitry/9/task2.py
+++ km73/Bondar_Dmitry/9/task2.py
@@ -3,7 +3,18 @@ n = int(input("Введіть степінь: "))
 
 
 def power(a, n):
-        return a**n
+        result = 1
+        if n == 0:
+                return result
+        elif n > 0:
+                for i in range(n):
+                        result = result * a
+        elif n < 0:
+                for i in range(abs(n)):
+                        result = result * (1/a)
+        return result
 
 
 print(power(a, n))
+
+
